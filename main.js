3b6d819d50f8c6fe08da168bbc11884b6fdbb035
Vladimir <vlad.secretario.boavista@gmail.com>
2026-08-26 12:23:50 -0300

    Ajuste de cards gerados por JS

--- main.js
+++ main.js
@@ -3,28 +3,59 @@
 
 const repositorios = [
   {
-    url,
-    nomeProjeto,
-    descricao,
-    miniatura,
+    url: "#testeURL",
+    nomeProjeto: "Teste nome do projeto",
+    descricao: "Teste descrição cweujvrjnvenjovenvenoevnjuevfvef",
+    miniatura:
+      "https://img.magnific.com/vetores-gratis/design-plano-design-de-logotipo-ac_23-2149482027.jpg?semt=ais_hybrid&w=740&q=80",
   },
   {},
   {},
+  {},
+  {},
+  {}
 ];
 
 const secaoProjetos = document.querySelector(".my-projects-list");
 
-
-function estruturador(){
+function estruturador() {
   repositorios.forEach((repositorio) => {
-    const card = document.createElement("div");
-    card.classList.add("card-project");
+    const cardProject = document.createElement("div");
+    cardProject.classList.add("card-project");
 
-    const cardButton = document.createElement("button");
+    
     const img = document.createElement("img");
+    img.classList.add("img-project")
+    img.src = repositorio.miniatura
+
+    const acordeom = document.createElement("button");
+    acordeom.classList.add("acordeom")
+
     const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body")
+
     const cardTitle = document.createElement("h3");
+    cardTitle.classList.add("card-title")
+    cardTitle.textContent = repositorio.nomeProjeto
+
     const cardDescription = document.createElement("p");
-    const cardNav = document.createElement("button")
-  })
+    cardDescription.classList.add("card-description")
+    cardDescription.textContent = repositorio.descricao
+
+    const cardNav = document.createElement("button");
+    cardNav.classList.add("card-button")
+    cardNav.textContent = "Acessar"
+
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardDescription);
+    cardBody.appendChild(cardNav);
+    cardProject.appendChild(img)
+    cardProject.appendChild(acordeom)
+    cardProject.appendChild(cardBody);
+
+    secaoProjetos.appendChild(cardProject)
+  });
 }
+
+estruturador()
