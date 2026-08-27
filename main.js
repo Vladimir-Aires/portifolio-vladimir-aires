801bc39e3b763851f5fb721f7208b11486c862f9
Vladimir <vlad.secretario.boavista@gmail.com>
2026-08-27 10:38:56 -0300

    Animação acordeom do card

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const repositorios = [
   {},
   {},
   {},
-  {}
+  {},
 ];
 
 const secaoProjetos = document.querySelector(".my-projects-list");
@@ -23,39 +23,46 @@ function estruturador() {
     const cardProject = document.createElement("div");
     cardProject.classList.add("card-project");
 
-    
     const img = document.createElement("img");
-    img.classList.add("img-project")
-    img.src = repositorio.miniatura
+    img.classList.add("img-project");
+    img.src = repositorio.miniatura;
 
     const acordeom = document.createElement("button");
-    acordeom.classList.add("acordeom")
+    acordeom.classList.add("acordeom");
+    acordeom.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
+
+    const icone = acordeom.querySelector("i");
+
+    acordeom.addEventListener("click", () => {
+      icone.classList.remove("icone-animando");
+      void icone.offsetWidth; // Força o reflow para reiniciar a animação
+      icone.classList.add("icone-animando");
+    });
 
     const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body")
+    cardBody.classList.add("card-body");
 
     const cardTitle = document.createElement("h3");
-    cardTitle.classList.add("card-title")
-    cardTitle.textContent = repositorio.nomeProjeto
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = repositorio.nomeProjeto;
 
     const cardDescription = document.createElement("p");
-    cardDescription.classList.add("card-description")
-    cardDescription.textContent = repositorio.descricao
+    cardDescription.classList.add("card-description");
+    cardDescription.textContent = repositorio.descricao;
 
     const cardNav = document.createElement("button");
-    cardNav.classList.add("card-button")
-    cardNav.textContent = "Acessar"
-
+    cardNav.classList.add("card-button");
+    cardNav.textContent = "Acessar";
 
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(cardDescription);
     cardBody.appendChild(cardNav);
-    cardProject.appendChild(img)
-    cardProject.appendChild(acordeom)
+    cardProject.appendChild(img);
+    cardProject.appendChild(acordeom);
     cardProject.appendChild(cardBody);
 
-    secaoProjetos.appendChild(cardProject)
+    secaoProjetos.appendChild(cardProject);
   });
 }
 
-estruturador()
+estruturador();
