28ab5cd3b550ae0ea862dafc01ba3d933e2d7d06
Vladimir <vlad.secretario.boavista@gmail.com>
2026-08-28 11:32:29 -0300

    Corrigindo html

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//reestruturação de código conforme a seguinte lógica:
-// vou salvar num array os objetos json dos principais repositórios que quero que apareçam no meu portifólio. Esses objetos vão aparecer como cards no html do meu repositório e vão ser gerados por doom
 
 const repositorios = [
   {
